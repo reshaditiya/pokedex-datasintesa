102334d0190bd65bc1be1018588105595ce0e836
reshaditiya <reshaditiya@gmail.com>
2023-09-03 22:26:46 +0700

    extract pokemon type

--- components/combobox-type.tsx
+++ components/combobox-type.tsx
@@ -16,41 +16,17 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
-import { useQuery } from '@tanstack/react-query';
 import { capitalizeFirstChar } from '@/lib/utils';
+import { pokemonTypes } from '@/data/pokemon-types';
 
-type PokemonTypesDb = {
-  count: number;
-  next: null;
-  previous: null;
-  results: { name: string; url: string }[];
-};
+const pokemonTypesCombo = pokemonTypes.sort().map((type) => ({
+  value: type,
+  label: capitalizeFirstChar(type),
+}));
 
 export function ComboboxType({ className }: { className?: string }) {
-  let pokemonTypes = [
-    {
-      value: 'allTypes',
-      label: 'All types',
-    },
-  ];
-
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(pokemonTypes[0].value);
-  const data = useQuery<PokemonTypesDb>({
-    queryKey: ['PokemonTypes'],
-    queryFn: () =>
-      fetch('https://pokeapi.co/api/v2/type').then((res) => res.json()),
-  });
-
-  if (data.data) {
-    pokemonTypes = [
-      ...pokemonTypes,
-      ...data.data.results.map((type) => ({
-        value: type.name,
-        label: capitalizeFirstChar(type.name),
-      })),
-    ];
-  }
+  const [value, setValue] = useState('');
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -62,8 +38,9 @@ export function ComboboxType({ className }: { className?: string }) {
           className="w-[200px] justify-between"
         >
           {value
-            ? pokemonTypes.find((pokemonTypes) => pokemonTypes.value === value)
-                ?.label
+            ? pokemonTypesCombo.find(
+                (pokemonType) => pokemonType.value === value,
+              )?.label
             : 'Select Type...'}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
@@ -73,9 +50,9 @@ export function ComboboxType({ className }: { className?: string }) {
           <CommandInput placeholder="Search type..." />
           <CommandEmpty>No type found.</CommandEmpty>
           <CommandGroup className="max-h-48 overflow-auto">
-            {pokemonTypes.map((pokemonTypes) => (
+            {pokemonTypesCombo.map((pokemonType) => (
               <CommandItem
-                key={pokemonTypes.value}
+                key={pokemonType.value}
                 onSelect={(currentValue) => {
                   setValue(currentValue === value ? '' : currentValue);
                   setOpen(false);
@@ -84,10 +61,10 @@ export function ComboboxType({ className }: { className?: string }) {
                 <Check
                   className={cn(
                     'mr-2 h-4 w-4',
-                    value === pokemonTypes.value ? 'opacity-100' : 'opacity-0',
+                    value === pokemonType.value ? 'opacity-100' : 'opacity-0',
                   )}
                 />
-                {pokemonTypes.label}
+                {pokemonType.label}
               </CommandItem>
             ))}
           </CommandGroup>
---
+++ data/pokemon-types.ts
@@ -0,0 +1,24 @@
+export const pokemonTypes = [
+  'normal',
+  'fighting',
+  'flying',
+  'poison',
+  'ground',
+  'rock',
+  'bug',
+  'ghost',
+  'steel',
+  'fire',
+  'water',
+  'grass',
+  'electric',
+  'psychic',
+  'ice',
+  'dragon',
+  'dark',
+  'fairy',
+  'unknown',
+  'shadow',
+];
+
+export type PokemonTypes = (typeof pokemonTypes)[number];
