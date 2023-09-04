08832fa2341a7226a6ff88e92b1fade71769ded4
reshaditiya <reshaditiya@gmail.com>
2023-09-04 14:21:22 +0700

    enhance fetch data pager

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,39 @@ import { ComboboxType } from '@/components/combobox-type';
 import PokemonCard from '@/components/pokemon-card';
 import { PokemonData, PokemonList } from '@/types/pokeapiDB.type';
 import { useQueries, useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 
-function useGetPokemons() {
-  //get list of pokemon
-  const getPokemonList = useQuery<PokemonList>(['pokemonList'], () =>
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=12&offset=0').then((data) =>
-      data.json(),
-    ),
+const DATA_PER_FETCH = 12;
+
+function useGetPokemons({
+  page,
+  setData,
+}: {
+  page: number;
+  setData: Function;
+}) {
+  // get list of pokemon
+  const {
+    data: getPokemonList,
+    refetch: refetchPokemonList,
+    isLoading: isLoadingPokemonList,
+  } = useQuery<PokemonList>(
+    ['pokemonList'],
+    () =>
+      fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=${DATA_PER_FETCH}&offset=${
+          (page - 1) * DATA_PER_FETCH
+        }`,
+      ).then((data) => data.json()),
+    { enabled: false },
   );
-  const pokemonList = getPokemonList.data?.results || [];
+
+  //get next pokemon when page change
+  useEffect(() => {
+    refetchPokemonList();
+  }, [page, refetchPokemonList]);
+
+  const pokemonList = getPokemonList?.results || [];
   //fetch all sub data from pokemon list
   const pokemonDatas = useQueries({
     queries: pokemonList.map((pokemon) => {
@@ -20,37 +44,43 @@ function useGetPokemons() {
         queryKey: ['pokemons', pokemon.name],
         queryFn: () => fetch(pokemon.url).then((data) => data.json()),
         staleTime: Infinity,
+        onSuccess: (data: PokemonData) => {
+          setData(data);
+        },
       };
     }),
   });
   //loading state on last sub data or pokemmon list
   const isLoading =
-    pokemonDatas[pokemonDatas.length - 1]?.isLoading ??
-    getPokemonList.isLoading;
+    pokemonDatas.every((val) => val === undefined) ?? isLoadingPokemonList;
 
   return {
-    data: [...pokemonDatas.map((pokemon) => pokemon.data)],
     isLoading: isLoading,
   };
 }
 
 export default function Home() {
-  const pokemonDatas = useGetPokemons();
-
-  console.log(pokemonDatas);
+  const [page, setPage] = useState(1);
+  const [pokemonDatas, setPokemonDatas] = useState<Array<PokemonData>>([]);
+  const { isLoading } = useGetPokemons({
+    page: page,
+    setData: (data: any) => {
+      setPokemonDatas((prev) => [...prev, data]);
+    },
+  });
 
   return (
     <main>
       <ComboboxType className="mt-6" />
       <section className="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {pokemonDatas.isLoading
+        {isLoading
           ? 'Loading...'
-          : pokemonDatas.data.map((pokemon) => (
+          : pokemonDatas.map((pokemon) => (
               <PokemonCard
                 key={pokemon.id}
                 name={pokemon.name}
                 types={pokemon.types.map((type: any) => type.type.name)}
-                image={pokemon.sprites.other['official-artwork'].front_default}
+                image={pokemon.sprites.other['official-artwork'].front_default!}
                 id={pokemon.id}
               />
             ))}
