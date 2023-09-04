135d9b5e4538f9ec2595e07aec87dfc667ec4aa4
reshaditiya <reshaditiya@gmail.com>
2023-09-04 15:16:14 +0700

    refactor useGetPokemon and move to new file

--- app/page.tsx
+++ app/page.tsx
@@ -2,72 +2,14 @@
 
 import { ComboboxType } from '@/components/combobox-type';
 import { PokemonCard } from '@/components/pokemon-card';
-import { PokemonData, PokemonList } from '@/types/pokeapiDB.type';
-import { useQueries, useQuery } from '@tanstack/react-query';
 import { useEffect, useState, useRef } from 'react';
 import { useIntersectionObserver } from 'usehooks-ts';
-
-const DATA_PER_FETCH = 12;
-
-function useGetPokemons({
-  page,
-  setData,
-}: {
-  page: number;
-  setData: Function;
-}) {
-  // get list of pokemon
-  const {
-    data: getPokemonList,
-    refetch: refetchPokemonList,
-    isLoading: isLoadingPokemonList,
-  } = useQuery<PokemonList>(
-    ['pokemonList'],
-    () =>
-      fetch(
-        `https://pokeapi.co/api/v2/pokemon?limit=${DATA_PER_FETCH}&offset=${
-          (page - 1) * DATA_PER_FETCH
-        }`,
-      ).then((data) => data.json()),
-    { enabled: false },
-  );
-
-  //get next pokemon when page change
-  useEffect(() => {
-    refetchPokemonList();
-  }, [page, refetchPokemonList]);
-
-  const pokemonList = getPokemonList?.results || [];
-  //fetch all sub data from pokemon list
-  const pokemonDatas = useQueries({
-    queries: pokemonList.map((pokemon) => {
-      return {
-        queryKey: ['pokemons', pokemon.name],
-        queryFn: () => fetch(pokemon.url).then((data) => data.json()),
-        staleTime: Infinity,
-        onSuccess: (data: PokemonData) => {
-          setData(data);
-        },
-      };
-    }),
-  });
-  //loading state on last sub data or pokemmon list
-  const isLoading =
-    pokemonDatas.every((val) => val === undefined) ?? isLoadingPokemonList;
-
-  return {
-    isLoading: isLoading,
-  };
-}
+import { useGetPokemons } from '../hooks/useGetPokemons';
 
 export default function Home() {
   const [page, setPage] = useState(1);
-  const [pokemonDatas, setPokemonDatas] = useState<Array<PokemonData>>([]);
-  const { isLoading } = useGetPokemons({
+  const { isLoading, pokemonDatas } = useGetPokemons({
     page: page,
-    setData: (data: any) => {
-      setPokemonDatas((prev) => [...prev, data]);
-    },
   });
   const lastElementRef = useRef<HTMLDivElement | null>(null);
   const lastElement = useIntersectionObserver(lastElementRef, {});
---
+++ hooks/useGetPokemons.tsx
@@ -0,0 +1,55 @@
+'use client';
+import { PokemonData, PokemonList } from '@/types/pokeapiDB.type';
+import { useQueries, useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+
+const DATA_PER_FETCH = 12;
+export function useGetPokemons({ page }: { page: number }) {
+  const [pokemonDatasState, setPokemonDatasState] = useState<
+    Array<PokemonData>
+  >([]);
+
+  // get list of pokemon
+  const {
+    data: getPokemonList,
+    refetch: refetchPokemonList,
+    isLoading: isLoadingPokemonList,
+  } = useQuery<PokemonList>(
+    ['pokemonList'],
+    () =>
+      fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=${DATA_PER_FETCH}&offset=${
+          (page - 1) * DATA_PER_FETCH
+        }`,
+      ).then((data) => data.json()),
+    { enabled: false },
+  );
+
+  //get next pokemon when page change
+  useEffect(() => {
+    refetchPokemonList();
+  }, [page, refetchPokemonList]);
+
+  const pokemonList = getPokemonList?.results || [];
+  //fetch all sub data from pokemon list
+  const pokemonDatas = useQueries({
+    queries: pokemonList.map((pokemon) => {
+      return {
+        queryKey: ['pokemons', pokemon.name],
+        queryFn: () => fetch(pokemon.url).then((data) => data.json()),
+        staleTime: Infinity,
+        onSuccess: (data: PokemonData) => {
+          setPokemonDatasState((prev) => [...prev, data]);
+        },
+      };
+    }),
+  });
+  //loading state on last sub data or pokemmon list
+  const isLoading =
+    pokemonDatas.every((val) => val === undefined) ?? isLoadingPokemonList;
+
+  return {
+    pokemonDatas: pokemonDatasState,
+    isLoading: isLoading,
+  };
+}
