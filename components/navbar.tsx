8b57479f4713efc590fc98f697a62029655d3931
reshaditiya <reshaditiya@gmail.com>
2023-09-04 20:00:53 +0700

    add link to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,23 @@
 import { Swords, Star } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
+import Link from 'next/link';
 
 export function Navbar({}) {
   return (
     <>
       <nav className="mt-6 flex items-center justify-between">
-        <span className="flex gap-3 text-lg font-semibold">
-          <Swords className="h-6 w-6" />
-          Pokedex
-        </span>
-        <Button variant="secondary" className="text-yellow-800">
-          <Star className="mr-2 h-4 w-4" /> My Favorite
-        </Button>
+        <Link href="/">
+          <span className="flex gap-3 text-lg font-semibold">
+            <Swords className="h-6 w-6" />
+            Pokedex
+          </span>
+        </Link>
+        <Link href="/favorite">
+          <Button variant="secondary" className="text-yellow-800">
+            <Star className="mr-2 h-4 w-4" /> My Favorite
+          </Button>
+        </Link>
       </nav>
       <Separator className="mt-6" />
     </>
