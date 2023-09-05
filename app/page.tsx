407376cba23374a9c15f00aa617ceca6383cf419
reshaditiya <reshaditiya@gmail.com>
2023-09-05 12:55:57 +0700

    refactor use infinite query

--- app/page.tsx
+++ app/page.tsx
@@ -2,44 +2,59 @@
 
 import { ComboboxType } from '@/components/combobox-type';
 import { PokemonCard } from '@/components/pokemon-card';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { PokemonList } from '@/types/pokeapiDB.type';
 import { useIntersectionObserver } from 'usehooks-ts';
-import { useGetPokemons } from '../hooks/useGetPokemons';
+
+const DATA_PER_FETCH = 12;
 
 export default function Home() {
-  const [page, setPage] = useState(1);
-  const { isLoading, pokemonDatas } = useGetPokemons({
-    page: page,
-  });
+  const { data, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery<PokemonList>(
+      ['pokemonList'],
+      ({ pageParam = 1 }) =>
+        fetch(
+          `https://pokeapi.co/api/v2/pokemon?limit=${DATA_PER_FETCH}&offset=${
+            (pageParam - 1) * DATA_PER_FETCH
+          }`,
+        ).then((data) => data.json()),
+      {
+        getNextPageParam: (_, pages) => {
+          return pages.length + 1;
+        },
+        initialData: {
+          pages: [],
+          pageParams: [1],
+        },
+      },
+    );
   const lastElementRef = useRef<HTMLDivElement | null>(null);
   const lastElement = useIntersectionObserver(lastElementRef, {});
-  const isVisible = !!lastElement?.isIntersecting;
 
   useEffect(() => {
-    if (!isLoading && isVisible) {
-      setPage((prev) => prev + 1);
-    }
-  }, [isVisible, isLoading]);
+    if (lastElement?.isIntersecting) fetchNextPage();
+  }, [lastElement, fetchNextPage]);
+
+  const pokemonListPages = data?.pages.flatMap((page) => page);
 
   return (
     <main>
       <ComboboxType className="mt-6" />
       <section className="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {isLoading
-          ? 'Loading...'
-          : pokemonDatas
-              .sort((a, b) => a.id - b.id)
-              .map((pokemon) =>
-                pokemon.id === pokemonDatas.length - 1 ? (
-                  <PokemonCard
-                    ref={lastElementRef}
-                    key={pokemon.id}
-                    pokemon={pokemon}
-                  />
-                ) : (
-                  <PokemonCard key={pokemon.id} pokemon={pokemon} />
-                ),
-              )}
+        {pokemonListPages?.map((pokemonList) =>
+          pokemonList.results.map((pokemon, i) =>
+            i === pokemonList.results.length - 1 ? (
+              <PokemonCard
+                ref={lastElementRef}
+                key={pokemon.name}
+                pokemonUrl={pokemon.url}
+              />
+            ) : (
+              <PokemonCard key={pokemon.name} pokemonUrl={pokemon.url} />
+            ),
+          ),
+        )}
       </section>
     </main>
   );
