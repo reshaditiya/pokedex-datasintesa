a928ef65839829e3bcfa5414e48a3a667522a86d
reshaditiya <reshaditiya@gmail.com>
2023-09-05 22:30:48 +0700

    code cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
         handleChange={setFilterType}
       />
       <section className="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {pokemonList.slice(0, DATA_PER_FETCH * page).map((pokemon, i) => (
+        {pokemonList.slice(0, DATA_PER_FETCH * page).map((pokemon) => (
           <PokemonCard key={pokemon.name} pokemonUrl={pokemon.url} />
         ))}
       </section>
