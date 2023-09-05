d11cf964c560ee17a8b5d46aa75bafa82930bec3
reshaditiya <reshaditiya@gmail.com>
2023-09-05 19:53:47 +0700

    fix filter type component

--- components/combobox-type.tsx
+++ components/combobox-type.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { useState } from 'react';
-import { Check, ChevronsUpDown } from 'lucide-react';
-import { cn } from '@/lib/utils';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { CheckSquare, ChevronsUpDown, RotateCcw, Square } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
   Command,
@@ -16,7 +15,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
-import { capitalizeFirstChar } from '@/lib/utils';
+import { capitalizeFirstChar, cn } from '@/lib/utils';
 import { pokemonTypes } from '@/data/pokemon-types';
 
 const pokemonTypesCombo = pokemonTypes.sort().map((type) => ({
@@ -24,52 +23,78 @@ const pokemonTypesCombo = pokemonTypes.sort().map((type) => ({
   label: capitalizeFirstChar(type),
 }));
 
-export function ComboboxType({ className }: { className?: string }) {
+export function ComboboxType({
+  className,
+  value,
+  handleChange,
+}: {
+  className?: string;
+  value: string[];
+  handleChange: Dispatch<SetStateAction<string[]>>;
+}) {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState('');
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild className={className}>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between"
-        >
-          {value
-            ? pokemonTypesCombo.find(
-                (pokemonType) => pokemonType.value === value,
-              )?.label
-            : 'Select Type...'}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput placeholder="Search type..." />
-          <CommandEmpty>No type found.</CommandEmpty>
-          <CommandGroup className="max-h-48 overflow-auto">
-            {pokemonTypesCombo.map((pokemonType) => (
-              <CommandItem
-                key={pokemonType.value}
-                onSelect={(currentValue) => {
-                  setValue(currentValue === value ? '' : currentValue);
-                  setOpen(false);
-                }}
-              >
-                <Check
-                  className={cn(
-                    'mr-2 h-4 w-4',
-                    value === pokemonType.value ? 'opacity-100' : 'opacity-0',
+    <div className={cn('flex items-center', className)}>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-full max-w-[16rem] justify-between"
+          >
+            <span className="truncate">
+              {value.length
+                ? value.map(
+                    (val, i) =>
+                      capitalizeFirstChar(val) +
+                      (i < value.length - 1 ? ', ' : ''),
+                  )
+                : 'Select Type...'}
+            </span>
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-full max-w-[16rem] p-0">
+          <Command>
+            <CommandInput placeholder="Search type..." />
+            <CommandEmpty>No type found.</CommandEmpty>
+            <CommandGroup className="max-h-56 overflow-auto">
+              {pokemonTypesCombo.map((pokemonType) => (
+                <CommandItem
+                  key={pokemonType.value}
+                  className="py-3"
+                  onSelect={(currentValue) => {
+                    handleChange((prev) =>
+                      prev.includes(currentValue)
+                        ? prev.filter((val) => val !== currentValue)
+                        : [...prev, currentValue],
+                    );
+                  }}
+                >
+                  {value.includes(pokemonType.value) ? (
+                    <CheckSquare className="mr-2 h-4 w-4" />
+                  ) : (
+                    <Square className="mr-2 h-4 w-4" />
                   )}
-                />
-                {pokemonType.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+                  {pokemonType.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+      {value.length > 0 && (
+        <Button
+          size="icon"
+          variant="secondary"
+          className="ml-2"
+          onClick={() => handleChange([])}
+        >
+          <RotateCcw className="h-4 w-4" />
+        </Button>
+      )}
+    </div>
   );
 }
