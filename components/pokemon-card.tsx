e3a382790cc887ae46340e211dd714aac2ff19d7
reshaditiya <reshaditiya@gmail.com>
2023-09-05 12:58:29 +0700

    refactor card fetch it's own data

--- components/pokemon-card.tsx
+++ components/pokemon-card.tsx
@@ -15,24 +15,36 @@ import { AspectRatio } from './ui/aspect-ratio';
 import { Ref, forwardRef } from 'react';
 import { PokemonData } from '@/types/pokeapiDB.type';
 import Link from 'next/link';
+import { useQuery } from '@tanstack/react-query';
 
 export const PokemonCard = forwardRef(function PokemonCard(
   {
-    pokemon,
+    pokemonUrl,
   }: {
-    pokemon: PokemonData;
+    pokemonUrl: string;
   },
   ref: Ref<HTMLDivElement>,
 ) {
+  const {
+    data: pokemonData,
+    isLoading: isLoadingPokemonData,
+    isError: isErrorPokemonData,
+  } = useQuery<PokemonData>(['pokemonData', pokemonUrl], () =>
+    fetch(pokemonUrl).then((data) => data.json()),
+  );
+
+  if (isLoadingPokemonData) return 'Loading...';
+  if (isErrorPokemonData) return 'Error';
+
   return (
     <Card ref={ref}>
       <CardHeader>
         <CardTitle className="text-lg font-semibold">
-          {capitalizeFirstChar(pokemon.name)}
-          <span className="ml-2 text-sm font-medium">#{pokemon.id}</span>
+          {capitalizeFirstChar(pokemonData.name)}
+          <span className="ml-2 text-sm font-medium">#{pokemonData.id}</span>
         </CardTitle>
         <div className="mt-2 flex gap-2">
-          {pokemon.types.map((type) => (
+          {pokemonData.types.map((type) => (
             <Badge variant="outline" key={type.type.name}>
               {capitalizeFirstChar(type.type.name)}
             </Badge>
@@ -42,21 +54,21 @@ export const PokemonCard = forwardRef(function PokemonCard(
       <CardContent>
         <AspectRatio ratio={1 / 1}>
           <Image
-            src={pokemon.sprites.other['official-artwork'].front_default!}
+            src={pokemonData.sprites.other['official-artwork'].front_default!}
             fill
-            alt={pokemon.name}
+            alt={pokemonData.name}
             className="object-cover"
           />
         </AspectRatio>
       </CardContent>
       <CardFooter className="flex justify-between">
         <Button variant="secondary" asChild>
-          <Link href={pokemon.name}>
+          <Link href={pokemonData.name}>
             <BookOpen className="mr-2 h-4 w-4" />
             See Detail
           </Link>
         </Button>
-        <FavoriteButton pokemon={pokemon} />
+        <FavoriteButton pokemonUrl={pokemonUrl} pokemon={pokemonData} />
       </CardFooter>
     </Card>
   );
